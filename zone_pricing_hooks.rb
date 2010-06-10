abeb5db2c2a27839a64f3ec60db652cf891ff4f2
Etienne van Tonder <etiennevt@gmail.com>
2010-06-10 11:42:33 +1000

    Complete admin refactor

--- zone_pricing_hooks.rb
+++ zone_pricing_hooks.rb
@@ -3,4 +3,7 @@ class ZonePricingHooks < Spree::ThemeSupport::HookListener
   # Add zone pricing tab to products admin area
   insert_after :admin_product_tabs, :partial => "admin/shared/zp_product_tab"
 
+  # Add zone pricing to variant edit form
+  insert_after :admin_variant_edit_form, 'admin/shared/zone_prices'
+
 end
