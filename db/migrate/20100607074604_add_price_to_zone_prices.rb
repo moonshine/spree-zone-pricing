37166892141d0c47bc510bf22354d7a76321f095
Etienne van Tonder <etiennevt@gmail.com>
2010-06-10 12:05:39 +1000

    Finalise front end changes

--- db/migrate/20100607074604_add_price_to_zone_prices.rb
+++
@@ -1,9 +0,0 @@
-class AddPriceToZonePrices < ActiveRecord::Migration
-  def self.up
-    add_column :zone_prices, :price, :decimal, :null => true, :default => nil, :precision => 8, :scale => 2
-  end
-
-  def self.down
-    remove_column :zone_prices, :price
-  end
-end
--- lib/spree/zone_pricing/products_helper.rb
+++ lib/spree/zone_pricing/products_helper.rb
@@ -13,12 +13,16 @@ module Spree::ZonePricing::ProductsHelper
     options.assert_valid_keys(:format_as_currency, :show_vat_text)
     options.reverse_merge! :format_as_currency => true, :show_vat_text => Spree::Config[:show_price_inc_vat]
 
+    # Check if this object is a product, if so then access the master variant
+    # record for the product to get the zone pricing
+    object = product_or_variant.is_a?(Product) ? product_or_variant.master : product_or_variant
+
     # Get the zone price for this variant/product if one is defined
     # otherwise use the normal price
-    if product_or_variant.respond_to?(:zone_price)
-      amount = product_or_variant.zone_price(get_user_country_id)
+    if object.respond_to?(:zone_price)
+      amount = object.zone_price(get_user_country_id)
     else
-      amount = product_or_variant.price
+      amount = object.price
     end
 
     amount += Calculator::Vat.calculate_tax_on(product_or_variant) if Spree::Config[:show_price_inc_vat]
